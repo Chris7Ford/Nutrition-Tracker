1eb82493c273fc078c2fe7b684aa3519f4bcea03
Christopher Ford <chrisf@page.works>
2021-07-31 13:09:03 -0400

    Create initial project Create create food page with modal for scanning Still in progress

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import NewFoodPage from "./pages/new-food";
+import Page from "./layouts/page/index";
+
+import './App.css';
+
+const App = () => (
+  <Page content={ NewFoodPage } />
+);
+
+export default App;
---
+++ src/layouts/page/index.js
@@ -0,0 +1,16 @@
+import Header from "../../components/navigation/header";
+import Container from 'react-bootstrap/Container';
+import "./page.css";
+
+const Page = (props) => {
+  return (
+    <>
+      <Header />
+      <Container className='mt-5'>
+      { props.content() }
+      </Container>
+    </>
+  );
+}
+
+export default Page;
